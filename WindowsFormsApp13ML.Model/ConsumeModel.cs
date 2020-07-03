f1dcb53da8b7248f97d99df358662a6837f9c37b
Patryk Krawczak <281923@office.umcs.pl>
2020-07-03 13:30:50 +0200

    aaaa

--- WindowsFormsApp13ML.Model/ConsumeModel.cs
+++ WindowsFormsApp13ML.Model/ConsumeModel.cs
@@ -27,7 +27,8 @@ namespace WindowsFormsApp13ML.Model
             MLContext mlContext = new MLContext();
 
             // Load model & create prediction engine
-            string modelPath = @"C:\Users\patryk.krawczak\AppData\Local\Temp\MLVSTools\WindowsFormsApp13ML\WindowsFormsApp13ML.Model\MLModel.zip";
+            string modelPath = @".\..\..\..\..\WindowsFormsApp13ML.Model\MLModel.zip"; //@"C:\Users\patryk.krawczak\source\repos\WindowsFormsApp13\WindowsFormsApp13ML.Model\MLModel.zip";
+
             ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
             var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
 
